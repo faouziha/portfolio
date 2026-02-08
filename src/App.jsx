b9511b7eaa215adca584cf0649f2af55cacc5a42
Faouzi haourigui <fhaourigui1@gmail.com>
2026-02-08 01:29:15 +0100

    Vercel analytics fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './pages/Home'
-import { inject } from '@vercel/analytics';
-
-inject();
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   
